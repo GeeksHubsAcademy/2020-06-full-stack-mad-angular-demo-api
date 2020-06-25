6f9b5ec6eda5f55d06720ec1a0a4c974bf003d2a
dave86dev <davidochando1986@gmail.com>
2020-06-25 12:33:47 +0200

    Conexión backend propuesta

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from 'src/app/service/movie.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private MovieService:MovieService) { }
 
   ngOnInit() {
   }
 
+  //función encargada de buscar la película por título
+  searchMovie(event){
+    //función de búsqueda
+    // if(event.target.value.length >= 3){
+    //   this.MovieService.getMoviesTitle(event.target.value)
+    //   .subscribe(res=>this.MovieService.setFilms(res))
+    // }
+  }
 }
--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -7,10 +7,18 @@ import { Observable } from 'rxjs';
 })
 export class MovieService {
 
+  //Urls de API (password(key) + url de la API)
+
   apiKey='210d6a5dd3f16419ce349c9f1b200d6d';
   apiUrl='https://api.themoviedb.org/3/';
+
+  //En el caso de que conectemos con un back.. el ejemplo a continuación....
+
+  backUrl='http://localhost:3000/films/';
+
   films: object[];
   filmChoosen: object[];
+  //title: string;
 
   constructor(private httpClient:HttpClient) { }
 
@@ -26,6 +34,12 @@ export class MovieService {
     
     return;
  }
+
+//  getMoviesTitle(title:string):Observable<any>{
+//    return this.httpClient.get(this.backUrl + 'title/' + title);
+//  }
+
+ 
   
 
 setFilms(films:object[]):void{
